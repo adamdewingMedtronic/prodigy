a9a958b46f06e666e17c907ed23a12d0f8fc6e1f
adamdewingMedtronic <adam.v.dewing@medtronic.com>
2020-07-29 14:17:13 -0500

    Added code for https.

---
+++ src/main/java/com/mdt/prodigy/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.mdt.prodigy.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure();
+        http.csrf().disable();
+    }
+
+}
